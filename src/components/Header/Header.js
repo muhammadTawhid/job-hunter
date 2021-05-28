c5b74fd8376053f512866553eff8daca052c33f5
mahfuz-antor <antorcnj2014@gmail.com>
2021-05-29 01:22:46 +0600

    Adding navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import header from '../../Images/header.jpg';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <img style={{width:'100%',height:'500px'}} src={header} alt="" />
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar d-flex  navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <a className="navbar-brand ms-5  text-white" href="/home"><h4>Donation</h4></a>
+
+            <button className="navbar-toggler bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link className="nav-link active me-5 text-white" aria-current="page" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link me-5 text-white" aria-current="page" to="/donate">Donate</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link me-5 text-white" aria-current="page" to="/viewOrder/:id">Events</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link me-5 text-white" aria-current="page" to="/dashboard">Dashboard</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link me-5 text-white" aria-current="page" to="/">Volanteers</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link me-5 text-white" aria-current="page" to="/">Contact</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    );
+};
+
+export default Navbar;
