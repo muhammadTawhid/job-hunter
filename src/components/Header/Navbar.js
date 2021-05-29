2654106ca9abab0e60d2cc9fedac827957a93e3d
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-05-29 23:30:01 +0600

    volunteer part complete

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,42 +1,86 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <nav className="navbar d-flex  navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-            <a className="navbar-brand ms-5  text-white ml-auto" href="/home"><h4 style={{fontSize:"2rem"}}><b>Donation</b></h4></a>
+  return (
+    <nav className="navbar d-flex  navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand ms-5  text-white ml-auto" href="/home">
+          <h4 style={{ fontSize: "2rem" }}>
+            <b>Donation</b>
+          </h4>
+        </a>
 
-            <button className="navbar-toggler bg-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
-
-                <ul className="navbar-nav">
-                    <li className="nav-item mr-4">
-                        <Link className="nav-link active me-5 text-white" aria-current="page" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item mr-4">
-                        <Link className="nav-link me-5 text-white" aria-current="page" to="/donate">Donate</Link>
-                    </li>
-                    <li className="nav-item mr-4">
-                        <Link className="nav-link me-5 text-white" aria-current="page" to="/viewOrder/:id">Events</Link>
-                    </li>
-                    {/* <li className="nav-item">
+        <button
+          className="navbar-toggler bg-info"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse flex-row-reverse"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item mr-4">
+              <Link
+                className="nav-link active me-5 text-white"
+                aria-current="page"
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="nav-item mr-4">
+              <Link
+                className="nav-link me-5 text-white"
+                aria-current="page"
+                to="/donate"
+              >
+                Donate
+              </Link>
+            </li>
+            <li className="nav-item mr-4">
+              <Link
+                className="nav-link me-5 text-white"
+                aria-current="page"
+                to="/viewOrder/:id"
+              >
+                Events
+              </Link>
+            </li>
+            {/* <li className="nav-item">
                         <Link className="nav-link me-5 text-white" aria-current="page" to="/dashboard">Dashboard</Link>
                     </li> */}
-                    <li className="nav-item mr-4">
-                        <Link className="nav-link me-5 text-white" aria-current="page" to="/">Volanteers</Link>
-                    </li>
-                    <li className="nav-item mr-5">
-                        <Link className="nav-link me-5 text-white" aria-current="page" to="/">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+            <li className="nav-item mr-4">
+              <Link
+                className="nav-link me-5 text-white"
+                aria-current="page"
+                to="/"
+              >
+                Volanteers
+              </Link>
+            </li>
+            <li className="nav-item mr-5">
+              <Link
+                className="nav-link me-5 text-white"
+                aria-current="page"
+                to="/"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
+      </div>
     </nav>
-    );
+  );
 };
 
 export default Navbar;
---
+++ src/components/Volanteer/Volanteer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import volunteer1 from "../../Images/volanteer1.jpg";
+import volunteer2 from "../../Images/volanteer2.jpg";
+import volunteer3 from "../../Images/volanteer3.jpg";
+import volunteer4 from "../../Images/volanteer4.jpg";
+import VolanteerCard from "../VolanteerCard/VolanteerCard";
+
+const Volunteer = () => {
+  const VolunteerData = [
+    {
+      img: volunteer1,
+      name: "Jhone Dove",
+    },
+    {
+      img: volunteer2,
+      name: "Merry Query",
+    },
+    {
+      img: volunteer3,
+      name: "David Watson",
+    },
+    {
+      img: volunteer4,
+      name: "Franklia Ats",
+    },
+  ];
+
+  return (
+    <div>
+      <div className="text-center">
+        <h3 style={{ textDecoration: "underline" }} className="text-white mt-5">
+          <b>OUR VOLUNTEERS</b>
+        </h3>
+      </div>
+      <div className="row justify-content-center ">
+        {VolunteerData.map((volanteer) => (
+          <VolanteerCard volanteer={volanteer} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Volunteer;
