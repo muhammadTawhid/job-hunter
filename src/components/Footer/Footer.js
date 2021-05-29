b24fd19e3c061ae23a5dc9c679ba030530b01480
mahfuz-antor <antorcnj2014@gmail.com>
2021-05-29 16:41:30 +0600

    testing purpose

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer page.</h1>
-        </div>
+        
+            <div style={{backgroundColor:'#44d699',color:'white'}}>
+                <h2  style={{paddingTop:'50px', textAlign:'center'}}>Welcome to our Contact section:</h2>
+                <div style={{marginLeft:'0',marginRight:'0'}} className="row p-3">
+                <div className="col-md-4">
+                    <h2>About:</h2>
+                </div>
+                <div className="col-md-4">
+                    <h2>Donations: </h2>
+                </div> 
+                <div className="col-md-4">
+                    <h2>Contact Us:</h2>
+                </div>
+
+            </div>
+            </div>
+        
     );
 };
 
